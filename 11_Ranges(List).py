53c2ba490eb99b2dffe485df9bd7ba170551fe2d
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-13 12:10:39 +0530

    Create 11_Ranges(List).py

---
+++ 11_Ranges(List).py
@@ -0,0 +1,11 @@
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+# print a range without adding manually
+
+my_range = range(1,21)
+print(list(my_range))
+
+my_range = []
+for i in range (1,21):
+  my_range.append(i)
+print(my_range)
