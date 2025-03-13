7c1226da314a4db8299bf15520ee8cd6cea6182a
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-13 12:27:52 +0530

    Create 15_Simple_dictionary.py

---
+++ 15_Simple_dictionary.py
@@ -0,0 +1,6 @@
+# Create a dictionary that contains the keys a  and b  and their respective values 1  and 2 .
+dic = {"a" : 1 , "b" : 2}
+print(dic)
+print(dic.keys())
+print(dic.values())
+print(dic.items())
