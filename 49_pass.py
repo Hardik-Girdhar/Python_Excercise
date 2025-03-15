3db583cdef88182a08e84968f2f01b2d133feff2
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-15 16:27:19 +0530

    Create 49_pass.py

---
+++ 49_pass.py
@@ -0,0 +1,10 @@
+# The code is supposed to get some input from the user, but instead, it produces an error. Please try to understand the error and then fix it.
+
+pass = input("Please enter your password: ") 
+
+# Answer: 
+pass1 = input("Please enter your password: ") 
+
+# Explanation:
+# There was a SyntaxError  error because the syntax to name the variable was wrong since pass  is a reserved keyword in Python.
+# However, you can solve that by adding a number to the name or simply choosing another variable name.
