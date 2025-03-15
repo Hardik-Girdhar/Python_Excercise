6bb8f5ad374da614ea6f40604a0e42b1b0b040e5
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-15 17:30:24 +0530

    Create 54_Modifying_Multilevel_Dictionaries(NEW).py

---
+++ 54_Modifying_Multilevel_Dictionaries(NEW).py
@@ -0,0 +1,14 @@
+# Question: Please update the dictionary by changing the last name of the second employee from Smith to Smooth or whatever takes your fancy.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+          
+# Answer: 
+d['employees'][1]['lastName'] = "Smooth"
+print(d)
+
+# Explanation:
+# On the left side of the assignment operator, we are accessing Smith  as we did in the previous exercise. Then using the assignment operator, we simply assign a new string to that item.
