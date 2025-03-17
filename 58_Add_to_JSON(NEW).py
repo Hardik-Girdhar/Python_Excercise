b18affbc2cd72383c381af897e28b28ac5412ff5
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-17 10:07:17 +0530

    Update 58_Add_to_JSON(NEW).py

--- 58_Add_to_JSON(NEW).py
+++ 58_Add_to_JSON(NEW).py
@@ -1,1 +1,7 @@
-s
+import json
+with open("json.txt","r+") as file:
+    d = json.loads(file.read())
+    d["employees"].append(dict(firstName="Hardik", lastName= "Girdhar"))
+    file.seek(0)
+    json.dump(d, file, indent =4, sort_keys = True)
+    file.truncate()
