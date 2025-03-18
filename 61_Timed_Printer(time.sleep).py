5b7321dfd8e8abe3d991ab70a5f9154e94a6fec2
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-18 09:24:31 +0530

    Create 61_Timed_Printer(time.sleep).py

---
+++ 61_Timed_Printer(time.sleep).py
@@ -0,0 +1,9 @@
+ # Create a program that prints out Hello  every two seconds.
+
+import time
+while(True):
+    print("hello")
+    time.sleep(2)
+
+# Explanation:
+# The sleep  method of the built-in time module suspends the execution of the script for the given number of seconds. 
