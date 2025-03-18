1756a2912af665255324a9d63ffa252d809e9fd0
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-18 09:37:51 +0530

    Create 65_Ignoring_next_statement.py

---
+++ 65_Ignoring_next_statement.py
@@ -0,0 +1,7 @@
+# w want to print hello repeatedly with out hi what should we can use here to print hello repeatedly 
+
+while True:
+    print("Hello")
+    if (2>1):
+        continue
+    print("Hi")
