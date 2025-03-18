59b7dce67b73912162177e658996811eea7fec31
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2025-03-18 09:35:57 +0530

    Create 64_Executing_next_statement.py

---
+++ 64_Executing_next_statement.py
@@ -0,0 +1,7 @@
+# Here it prints Hello as an output but we want hi as well as in output what should we use at place of break so that it can print hello with hi
+
+while True:
+    print("Hello")
+    if (2>1):
+        pass
+    print("Hi")
